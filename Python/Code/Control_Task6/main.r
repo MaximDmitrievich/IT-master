e6c93682a6060344e39e040442da680129ed1a1e
Maxim Eremin <mderemin@gmail.com>
2019-12-07 18:27:11 +0300

    Fixed Control Task 6

--- Python/Code/Control_Task6/main.r
+++ Python/Code/Control_Task6/main.r
@@ -1,8 +1,11 @@
 f <- function(vec, pattern, how_much) {
   res <- c()
   patterns_idx <- which(vec == pattern)
-  for (i in 1:(length(patterns_idx) - 1)) {
-    if (patterns_idx[i + 1] - patterns_idx[i] == how_much - 1) {
+  window <- (how_much - 1)
+  print(patterns_idx)
+  for (i in 1:(length(patterns_idx) - window)) {
+    nxt <- (i + window)
+    if ((patterns_idx[nxt] - patterns_idx[i]) == window) {
       res <- append(res, patterns_idx[i])
     }
   }
@@ -10,3 +13,5 @@ f <- function(vec, pattern, how_much) {
 }
 
 print(f(c(1,1,2,3,2,2,4,2,2,2), 2, 2))
+print(f(c(1,1,2,3,2,2,4,2,2,2), 2, 3))
+print(f(c(1,1,2,3,2,2,4,2,2,2,4,4,4,4), 4, 4))
